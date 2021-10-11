9ad3f69e521d7d995c6e1174e742dea6d35449c5
ALQAISI yasir <yazraf2@gmail.com>
2021-10-11 14:05:48 +0200

    chenge env

--- src/env.js
+++ src/env.js
@@ -4,7 +4,7 @@ const env = () => {
   }
 
   if (process.env.NODE_ENV === 'production') {
-    return 'https://api.yaz-fr.com/';
+    return 'https://yaz-fr.com/api.yaz-fr.com/';
   }
   return '';
 };
