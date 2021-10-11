dac3133ea8d2dcefd24a6aadc5e05756cd0d16d6
ALQAISI yasir <yazraf2@gmail.com>
2021-10-11 14:38:00 +0200

    add back

---
+++ src/env.js
@@ -0,0 +1,12 @@
+const env = () => {
+  if (process.env.NODE_ENV === 'development') {
+    return 'https://yaz-fr.com/api.yaz-fr.com/';
+  }
+
+  if (process.env.NODE_ENV === 'production') {
+    return 'https://yaz-fr.com/api.yaz-fr.com/';
+  }
+  return '';
+};
+
+export default env;
